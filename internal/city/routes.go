e9c8a166d9007aab51d3c15c319496b4702f7c11
Abdulfetah <suudiabdulfetah@gmail.com>
2025-07-16 01:12:24 +0300

    feat: Add some city routes

---
+++ internal/city/routes.go
@@ -0,0 +1,20 @@
+package city
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/iamsuudi/digital-id-server/internal/auth"
+	"github.com/iamsuudi/digital-id-server/internal/repository"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func RegisterRoutes(rg *gin.RouterGroup, dbConn *pgxpool.Pool, dbQueries *repository.Queries) {
+	service := NewService(dbConn, dbQueries)
+	handler := NewHandler(service)
+
+	residentGroup := rg.Group("/cities", auth.AuthMiddleware())
+	{
+		residentGroup.POST("/create", handler.CreateCity)
+		residentGroup.GET("/:id", handler.GetCity)
+		residentGroup.GET("/", handler.GetAll)
+	}
+}
